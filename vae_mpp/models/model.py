8d2f1a07eb36ad921b50c7320d9b8ea7ba00396d
Alex Boyd <ajboyd@calpoly.edu>
2019-10-06 21:42:34 -0700

    New optimization initialization.

--- vae_mpp/models/model.py
+++ vae_mpp/models/model.py
@@ -1,8 +1,44 @@
-from torch.nn import Module
+from torch import nn
 
-from vae_mpp.utils import Registrable
+NORMS = (
+    nn.LayerNorm,
+    nn.BatchNorm1d,
+    nn.BatchNorm2d,
+    nn.BatchNorm3d,
+    nn.GroupNorm,
+    nn.InstanceNorm1d,
+    nn.InstanceNorm2d,
+    nn.InstanceNorm3d,
+    nn.LocalResponseNorm,
+    #nn.SyncBatchNorm,
+)
 
-class Model(Module, Registrable):
+class Model(nn.Module):
 
-    def __init__(self ):
+    def __init__(self):
         super().__init__()
+    
+    def get_param_groups(self):
+        '''Returns iterable of dictionaries specifying parameter groups.
+        The first dictionary in the return value contains parameters that will be subject to weight decay.
+        The second dictionary in the return value contains parameters that will not be subject to weight decay.
+        '''
+
+        weight_decay_params = {'params': []}
+        no_weight_decay_params = {'params': [], 'weight_decay': 0.0}
+        for module_ in self.modules():
+            # Doesn't make sense to decay weights for a LayerNorm, BatchNorm, etc.
+            if isinstance(module_, NORMS):
+                no_weight_decay_params['params'].extend([
+                    p for p in module_._parameters.values() if p is not None
+                ])
+            else:
+                # Also doesn't make sense to decay biases.
+                weight_decay_params['params'].extend([
+                    p for n, p in module_._parameters.items() if p is not None and n != 'bias'
+                ])
+                no_weight_decay_params['params'].extend([
+                    p for n, p in module_._parameters.items() if p is not None and n == 'bias'
+                ])
+
+        return weight_decay_params, no_weight_decay_params
--- vae_mpp/optim.py
+++ vae_mpp/optim.py
@@ -1,23 +1,99 @@
 import torch
+import math
 
-from vae_mpp.utils import Registrable
-
-
-class Optimizer(Registrable, torch.optim.Optimizer):
-    pass
-
-
-Registrable._registry[Optimizer] = {
-    "adam": torch.optim.Adam,
+'''
+Dictionary of supported optimization algorithms.
+'''
+OPTIMIZERS = {
     "sgd": torch.optim.SGD,
+    "adadelta": torch.optim.Adadelta,
+    "adam": torch.optim.Adam,
+    "adagrad": torch.optim.Adagrad,
+    "rmsprop": torch.optim.RMSprop,
 }
 
 
-class LRScheduler(Registrable, torch.optim.lr_scheduler._LRScheduler):
-    pass
+class LRScheduler(torch.optim.lr_scheduler._LRScheduler):
+    '''Learning rate scheduler that is capable of a warm up period, and decaying the learning rate to zero.
+    Implementation derived from https://github.com/NVIDIA/Megatron-LM/blob/master/learning_rates.py'''
+
+    # Dictionary of supported learning rate decay schedules.
+    # Each value is a function with domains and ranges of: [0, 1] --> [0, 1]
+    DECAY_STYLES = {
+        "constant": lambda x: 1.0,
+        "linear": lambda x: 1.0 - x,
+        "cosine": lambda x: 0.5 * (1 + math.cos(math.pi * x)),
+        #"exponential": lambda x: pass,
+    }
+
+    def __init__(self, optimizer, start_lr, warmup_iter, num_iters, decay_style):
+        self.optimizer = optimizer
+        self.start_lr = float(start_lr)
+        self.warmup_iter = warmup_iter
+        self.num_iters = 0  # current step
+        self.end_iter = num_iters
+        self.decay_style = decay_style
+        self.decay_func = LRScheduler.DECAY_STYLES[decay_style]
+        self.step(self.num_iters)
+
+    def get_lr(self):
+        # Ramp up linearly if we are in the warmup period
+        if self.warmup_iter > 0 and self.num_iters <= self.warmup_iter:
+            return self.start_lr * self.num_iters / self.warmup_iter
+        else:
+            pct_step = (self.num_iters - self.warmup_iter) / (self.end_iter - self.warmup_iter)
+            return self.start_lr * self.decay_func(pct_step)
+
+    def step(self, step_num=None):
+        if step_num is None:
+            step_num = self.num_iters + 1
+        self.num_iters = step_num
+        new_lr = self.get_lr()
+        for group in self.optimizer.param_groups:
+            group['lr'] = new_lr
+
+    def state_dict(self):
+        sd = {
+                'start_lr': self.start_lr,
+                'warmup_iter': self.warmup_iter,
+                'num_iters': self.num_iters,
+                'decay_style': self.decay_style,
+                'end_iter': self.end_iter
+        }
+        return sd
+
+    def load_state_dict(self, sd):
+        self.start_lr = sd['start_lr']
+        self.warmup_iter = sd['warmup_iter']
+        self.num_iters = sd['num_iters']
+        self.end_iter = sd['end_iter']
+        self.decay_style = sd['decay_style']
+        self.decay_func = LRScheduler.DECAY_STYLES[sd['decay_style']]
+        self.step(self.num_iters)
 
 
-Registrable._registry[LRScheduler] = {
-    "lambda": torch.optim.lr_scheduler.LambdaLR,
-    "step": torch.optim.lr_scheduler.StepLR
-}
+def get_optimizer(model, args):
+
+    param_groups = model.get_param_groups()
+
+    optimizer = OPTIMIZERS[args["train"]["optimizer"]["kind"]](
+        param_groups,
+        **args["train"]["optimizer"]["args"],  # learning rate, weight decay, etc.
+    )
+
+    return optimizer
+    
+def get_lr_scheduler(optimizer, args):
+    
+    total_iterations = args["train"]["learning_rate"]["train_iters"]
+    warmup_iterations = math.floor(args["train"]["learning_rate"]["warmup_pct"] * total_iterations)
+
+    lr_scheduler = LRScheduler(
+        optimizer=optimizer,
+        start_lr=args["train"]["learning_rate"]["start_lr"],
+        warmup_iter=warmup_iterations,
+        num_iters=total_iterations,
+        decay_style=args["train"]["learning_rate"]["decay_style"],
+    )
+
+    return lr_scheduler
