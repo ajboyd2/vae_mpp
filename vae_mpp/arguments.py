3f05576bde9df84468185f25a1cd005541c6d46f
Alex Boyd <ajboyd@calpoly.edu>
2019-10-07 19:34:22 -0700

    Added arguments and updated references to said arguments.

--- vae_mpp/arguments.py
+++ vae_mpp/arguments.py
@@ -6,7 +6,7 @@ from .utils import print_log
 
 def general_args(parser):
     group = parser.add_argument_group("General set arguments for miscelaneous utilities.")
-    group.add_argument("--json_config_path", default=None, help="Path to json file containing arguments to be parsed.")
+    #group.add_argument("--json_config_path", default=None, help="Path to json file containing arguments to be parsed.")
     #group.add_argument("--", default=, help="")
 
 def model_config_args(parser):
@@ -15,7 +15,17 @@ def model_config_args(parser):
 
 def training_args(parser):
     group = parser.add_argument_group("Training specification arguments.")
-    #group.add_argument("--", default=, help="")
+    group.add_argument("--checkpoint_path", default="./", help="")
+    group.add_argument("--train_iters", default=10000, help="Number of batches to iterate over for training.")
+    group.add_argument("--batch_size", default=32, help="Number of samples per batch.")
+    group.add_argument("--valid_iters", default=500, help="Number of batches to iterate over each validation period.")
+    group.add_argument("--valid_interval", default=1000, help="Number of training batches to complete before starting a validation period")
+    group.add_argument("--save_interval", default=2000, help="Number of training batches to complete before saving a model checkpoint.")
+    group.add_argument("--optimizer", default="adam", help="Type of optimization algorithm to use.")
+    group.add_argument("--lr", default=0.0003, help="Learning rate.")
+    group.add_argument("--weight_decay", default=0.01, help="L2 coefficient for weight decay.")
+    group.add_argument("--warmup_pct", default=0.01, help="Percentage of 'train_iters' to be spent ramping learning rate up from 0.")
+    group.add_argument("--lr_decay_style", default="cosine", help="Decay style for the learning rate, after the warmup period.")
 
 def evaluation_args(parser):
     group = parser.add_argument_group("Evaluation specification arguments.")
@@ -24,10 +34,6 @@ def evaluation_args(parser):
 def sampling_args(parser):
     group = parser.add_argument_group("Sampling specification arguments.")
     #group.add_argument("--", default=, help="")
-
-def load_args(args):
-    '''Override key/value pairs in args dictionary with those in the json config file.'''
-    args.update(json.load(open(args.json_config_path, "r")))
 
 def print_args(args):
     max_arg_len = max(len(k) for k in args.keys())
@@ -48,9 +54,6 @@ def get_args():
     sampling_args(parser)
 
     args = parser.parse_args()
-
-    if args.json_config_path:
-        load_args(args)
 
     if args.print_args:
         print_args(args)
--- vae_mpp/optim.py
+++ vae_mpp/optim.py
@@ -76,24 +76,25 @@ def get_optimizer(model, args):
 
     param_groups = model.get_param_groups()
 
-    optimizer = OPTIMIZERS[args["train"]["optimizer"]["kind"]](
+    optimizer = OPTIMIZERS[args["optimizer"]](
         param_groups,
-        **args["train"]["optimizer"]["args"],  # learning rate, weight decay, etc.
+        lr=args["lr"],
+        weight_decay=args["weight_decay"],  # learning rate, weight decay, etc.
     )
 
     return optimizer
     
 def get_lr_scheduler(optimizer, args):
     
-    total_iterations = args["train"]["learning_rate"]["train_iters"]
-    warmup_iterations = math.floor(args["train"]["learning_rate"]["warmup_pct"] * total_iterations)
+    total_iterations = args["train_iters"]
+    warmup_iterations = math.floor(args["warmup_pct"] * total_iterations)
 
     lr_scheduler = LRScheduler(
         optimizer=optimizer,
-        start_lr=args["train"]["learning_rate"]["start_lr"],
+        start_lr=args["lr"],
         warmup_iter=warmup_iterations,
         num_iters=total_iterations,
-        decay_style=args["train"]["learning_rate"]["decay_style"],
+        decay_style=args["lr_decay_style"],
     )
 
     return lr_scheduler
