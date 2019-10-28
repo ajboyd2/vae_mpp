471504308f32b7d8995fd2bd210ec82edf50afae
Alex Boyd <ajboyd@calpoly.edu>
2019-10-27 18:03:54 -0700

    Added aggregator to init file.

--- vae_mpp/modules/__init__.py
+++ vae_mpp/modules/__init__.py
@@ -1,3 +1,4 @@
 from vae_mpp.modules.decoder import PPDecoder
 from vae_mpp.modules.encoder import PPEncoder
+from vae_mpp.modules.aggregator import PPAggregator
 from vae_mpp.modules.time_embedding import TimeEmbedding
